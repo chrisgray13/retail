fde89f8c0e90d01abc69b0fe1ab4216b89d69da1
Chris Gray <chris_s_gray@hotmail.com>
2012-12-28 09:06:47 -0500

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Retail::Application.config.secret_token = '3178113c7f743cce6a957a856e6e85af7ceced871d4e3bbcab84c4dab55055f44ecb829b94f9fac55915822f00e5fdb4074299f62122b2c67b982e0b890604cf'
